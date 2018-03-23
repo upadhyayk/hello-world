27e029edcc61354935238ed13d42750d930d861a
j2payton <payton@temple.edu>
2018-03-23 17:12:14 -0400

    updates to lab 1 solution

---
+++ rpc/lock_server.h
@@ -0,0 +1,34 @@
+// this is the lock server
+// the lock client has a similar interface
+
+#ifndef lock_server_h
+#define lock_server_h
+
+#include <string>
+#include "lock_protocol.h"
+#include "lock_client.h"
+#include "rpc.h"
+
+class lock_server {
+
+ protected:
+  int nacquire;
+  pthread_mutex_t mutex;
+  std::map<lock_protocol::lockid_t, lock* > lockmap;
+
+ public:
+  lock_server();
+  ~lock_server() {};
+  lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
+};
+
+#endif 
+
+
+
+
+
+
+
